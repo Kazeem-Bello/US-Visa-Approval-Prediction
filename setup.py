fa4a5c5e6c04eeff1cd5de787c9f16843bdfa475
Kazeem-Bello <140184555+Kazeem-Bello@users.noreply.github.com>
2024-05-06 08:00:38 +0100

    requirements aded

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "US_Visa",
+    version = "0.0.0",
+    author = "Kazeem",
+    author_email = "bel.kazeem@gmail.com",
+    packages = find_packages()
+)
