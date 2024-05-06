5be6f8f153994c9b907b2e322dc86231eade5023
Kazeem-Bello <140184555+Kazeem-Bello@users.noreply.github.com>
2024-05-06 05:54:06 +0100

    Created folder structure

---
+++ template.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+
+project_name = "US_Visa"
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    # f"{project_name}/configuration/s3_operations.py",
+    f"{project_name}/constant/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    f"{project_name}/pipeline/train_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    # f"{project_name}/ml/__init__.py",
+    # f"{project_name}/ml/features.py",
+    # f"{project_name}/ml/models.py",
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "config/model.yaml",
+    "config/schema.yaml"
+]
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    filedir, filename = os.path.split(file_path)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass
+    else:
+        print(f"File {file_path} already exists")
+
