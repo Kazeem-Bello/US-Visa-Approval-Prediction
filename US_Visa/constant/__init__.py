4136aed9bbbd2d823765532568b36408afd79f2c
Kazeem-Bello <140184555+Kazeem-Bello@users.noreply.github.com>
2024-06-18 14:14:32 +0100

    data validation started

--- US_Visa/constant/__init__.py
+++ US_Visa/constant/__init__.py
@@ -26,12 +26,16 @@ train_file_name: str = "train.csv"
 test_file_name: str = "test.csv"
 
 # Data Ingestion related constant 
-
 data_ingestion_collection_name: str = "us_visa-data"
 data_ingestion_dir_name: str = "data_ingestion"
 data_ingestion_feature_store_dir: str = "feature_store"
 data_ingestion_ingested_dir: str = "ingested"
 data_ingestion_train_test_split: float = 0.2
+
+# Data Validation related constant
+data_validation_dir_name = "data validation"
+data_validation_drift_report_dir = "drift_report"
+data_validation_drift_report_file_name = "report.yaml"
 
 
 
